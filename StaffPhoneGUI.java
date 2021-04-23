c618a120071cb2f44e02b452d1ed226b5118636d
Fahad Baloch <76732084+Fahad-Baloch1@users.noreply.github.com>
2021-04-23 23:18:22 +0500

    Update StaffPhoneGUI.java

--- StaffPhoneGUI.java
+++ StaffPhoneGUI.java
@@ -1,27 +1,3 @@
-/*
-3.  Write a Java program using GUI that contains a text area and a command button titled as “Display”. When the “Display” button is clicked, the program has to read data from an existing data file named "staffphone.txt" and displays all the data in the text area properly. The data in the file "staffphone.txt" has the following format:
-John Pearson, 49308877
-Peter London, 49446701
-Amy Andersen, 45009923
-
-In addition to the data, the text area has to display the total number of the phone entries at the end.
-
-4. (Continued from Question 3). This time when the program starts to run, the data file is automatically loaded and all the phone entries from the data file (staffphone.txt) are displayed on the text area as shown below. The GUI has two extra buttons – one named as “Add” and the other one named as “Save”.  When the “Add” button is clicked, an input dialog is asks the user to input a name; and similar input message dialog for obtaining phone value. The newly obtained data values have to be appended to the text area. If the “Save” button is clicked, all data shown on the text area has to be stored to the data file "staffphone.txt" in a suitable format.  Hint: You may need to use file read/write. The “Clear” button can be used to clear the contents from the text area.
-*/
-/*
-
-Step 1: Story Board
-Step 2: Build GUI
-Step 3: Read file and display in text area
-Step 4: Tweaks and improvements
-Read data properly
-Staff class
-ArrayList
-
-Validation
-
-*/
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
